ac712653af5f2497fc33f0e5051545e56e7e58be
Janne Spijkervet <janne.spijkervet@hollandfoodz.com>
2019-07-23 16:32:55 +0200

    Added logging message

--- main.py
+++ main.py
@@ -153,3 +153,5 @@ if __name__ == '__main__':
     if exit_code:
         error_msg = 'An import from the Boltrics (Zeewolde) system failed.\n\nPlease contact IT to review the following error:\n\n{}'.format(error_msg)
         mail(SMTP_SERVER, SENDER_EMAIL, RECEIVER_EMAIL, SENDER_PASSWORD, error_msg)
+    else:
+        logging.info("Completed successfully")
